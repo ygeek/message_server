0ecd4e7fddf09a7609f05d6c27858a18ddb8d0b4
nightfade <nightfade.blue@gmail.com>
2017-02-15 23:36:20 +0800

    MOD: use docker stack

--- build-image.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-. config-default.sh
-docker build -t $imageName .
--- config-default.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-imageName=chareice/message_server #$(git rev-parse --short HEAD)
-containerName=message_server
